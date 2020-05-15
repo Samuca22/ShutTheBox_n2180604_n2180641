b613e29236756baf7d06815186db0aba32482019
ricardofm99 <32984426+ricardofm99@users.noreply.github.com>
2020-05-15 18:18:53 +0100

    Pasta webapp apagada

--- weblogicmvc-shutthebox/shutthebox/model/Score.php
+++ weblogicmvc-shutthebox/shutthebox/model/Score.php
@@ -9,11 +9,11 @@ class Score extends Model{
         array('user', 'class_name'=>'User')
     );
     
-    // public static function registarScore($id){
-    //     $score = new Score();
-    //     $score->pontuacao = "preencher com a pontuacao";
-    //     $score->dataJogo = date('d-m-Y H:i:s');
-    //     $score->userid = $id;
-    //     $pontuacao->save();
-    // }
+    //public static function registarScore($id){
+    //    $score = new Score();
+    //    $score->pontuacao = "preencher com a pontuacao";
+    //    $score->dataJogo = date('d-m-Y H:i:s');
+    //    $score->userid = $id;
+    //    $pontuacao->save();
+    //}
 };
